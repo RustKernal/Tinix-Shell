c61fd097da13ba02a7e391b3be387698b55a93d1
npex42 <npex42@gmail.com>
2021-05-11 11:06:28 +0100

    Fixing Tests

--- src/lib.rs
+++
@@ -1,4 +0,0 @@
-//Copyright 2021, George Venn, GPL v3.0, NO WARRANTY
-
-//We can't use the Rust Standard Library as a Baremetal Application
-#![no_std]
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@
 #![no_std]
 //We also can't use the normal Main entry point
 #![no_main] 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 //Imports
 use core::panic::PanicInfo;
+use tinix_kernal::qemu::{QemuExitCode, exit_qemu};
 
 //We dont want to mangle the name of this function in export
 #[no_mangle]
@@ -19,4 +23,8 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 pub fn panic_handler(_info : &PanicInfo) -> ! {
     loop {}
+}
+
+pub fn test_runner(_tests: &[&dyn Fn()]) {
+    exit_qemu(QemuExitCode::Success);
 }
